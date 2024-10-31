450af39055a7251bbd65cb2ee521dbb60800ac16
iago <iago@lakatos>
2024-10-31 17:06:45 -0300

    adicionando typebot na pagina

--- blueprints/page.py
+++ blueprints/page.py
@@ -1,7 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, request, jsonify, render_template
 
 page = Blueprint('page', __name__)
 
 @page.route('/')
 def home():
     return render_template('home.html')
+
+@page.route('/teste', methods=['POST'])
+def receber_dados():
+    dados = request.json
+    nome = dados.get("nome")
+    email = dados.get("email")
+    telefone = dados.get("telefone")
+    print(f"Nome: {nome}, Email: {email}, Telefone: {telefone}")
+    return jsonify({"status": "sucesso", "mensagem": "Dados recebidos com sucesso"}), 200
